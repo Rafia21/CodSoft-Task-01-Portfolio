ac7ec3ea7abb82197a420334628e21ff21c77f3d
Rafia21 <rafiaziz450@gmail.com>
2023-09-17 12:07:07 +0500

    1st commit
    Building Portfolio using HTML,CSS and JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// var typed=new typed(".text",
+// {
+//     Strings:["FrontEnd-Developer","BackEnd-Developer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
